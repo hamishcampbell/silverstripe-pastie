21d8da27d5aec27b05ed343f889d1fbf4d3cb781
Christian Bachhelm <bachhelmc@p7media.de>
2011-02-02 14:21:33 +0100

    ENHANCEMENT Added german language file

---
+++ lang/de_DE.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * German language pack
+ * @package pastie
+ * @subpackage i18n
+ */
+
+global $lang;
+
+$lang['de_DE']['PastieSnippet']['TITLE'] = 'Titel';
+$lang['de_DE']['PastieSnippet']['LANGUAGE'] = 'Sprache';
+$lang['de_DE']['PastieSnippet']['CONTENT'] = 'Inhalt';
+$lang['de_DE']['PastieSnippet']['SAVE'] = 'Speichern';
+$lang['de_DE']['PastieSnippet']['ANYONE'] = 'Jeder';
+$lang['de_DE']['PastieSnippet']['LOGGEDINUSERS'] = 'Eingeloggte Benutzer';
+$lang['de_DE']['PastieSnippet']['SPECIFICUSERS'] = 'Ausgew&auml;hlte Benutzer';
+$lang['de_DE']['PastieSnippet']['WHOCANCREATE'] = 'Wer darf Snippets erstellen?';
+$lang['de_DE']['PastieSnippet']['SPECIFICGROUPS'] = 'Ausgew&auml;hlte Benutzer Gruppen';
+$lang['de_DE']['PastieSnippet']['MAXCONTENTLENGTH'] = 'Maximale Inhaltsl&auml;nge';
+$lang['de_DE']['PastieSnippet']['SINGULARNAME'] = 'Snippet';
+$lang['de_DE']['PastieSnippet']['PLURALNAME'] = 'Snippets';
+$lang['de_DE']['PastieSnippet']['CREATENEW'] = 'Neues Snippet erstellen';
+$lang['de_DE']['PastieSnippet']['CREATECHILD'] = 'Kind-Snippet erstellen';
+
+$lang['de_DE']['PastieSnippet']['CONTENTEMPTYERROR'] = 'Inhalt darf nicht leer sein.';
+$lang['de_DE']['PastieSnippet']['CONTENTTOOLARGE'] = 'Inhalt &uuml;berschreitet die Maximall&auml;nge.';
+$lang['de_DE']['PastieSnippet']['SELECTVALIDLANGUAGE'] = 'Bitte g&uuml;ltige Sprache ausw&auml;hlen.';
+$lang['de_DE']['PastieSnippet']['SAVEDNEW'] = 'Neues Snippet erstellt';
+
+$lang['de_DE']['PastiePage.ss']['LATESTSNIPPETS'] = 'Letzte Snippets';
+$lang['de_DE']['PastiePage.ss']['NOSNIPPETSFOUND'] = 'Keine Snippets gefunden.';
+
+$lang['de_DE']['PastiePage_show.ss']['PARENT'] = 'Eltern';
+$lang['de_DE']['PastiePage_show.ss']['CHILD'] = 'Kind';
+$lang['de_DE']['PastiePage_show.ss']['NOTFOUND'] = 'Snippet nicht gefunden';
+
+$lang['de_DE']['PastieSnippetTitle.ss']['UNTITLED'] = 'Ohne Titel';
+$lang['de_DE']['PastieSnippetTitle.ss']['CREATED'] = 'Erstellt';
+$lang['de_DE']['PastieSnippetTitle.ss']['BY'] = 'von'
+
+?>
